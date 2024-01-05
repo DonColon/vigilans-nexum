ec7295a480d1a7bf924298c4c2c5e7658b014d1c
DonColon <drrafshi@hotmail.de>
2024-01-06 00:31:25 +0100

    Create release when version in package.json changes

--- .github/scripts/release-app.js
+++ .github/scripts/release-app.js
@@ -1,5 +1,42 @@
 import pkg from "../../package.json" assert { type: "json" };
 
-export default async ({ core, context }) => {
-	core.info(`version: ${pkg.version}`);
+export default async ({ core, context, github }) => {
+	const appVersion = `v${pkg.version}`;
+	const { owner, repo } = context.repo;
+
+	const { data: [latestRelease]  } = await github.rest.repos.listReleases({
+		owner,
+		repo,
+		per_page: 1
+	});
+
+	let isFirstRelease = false;
+	let previousVersion = appVersion;
+
+	if (latestRelease) {
+		previousVersion = latestRelease.tag_name;
+		core.info(`Previous release version: ${previousVersion}`);
+	} else {
+		isFirstRelease = true;
+		core.info(`First release version: ${appVersion}`);
+	}
+
+	if (isFirstRelease || appVersion > previousVersion) {
+		const { data } = await github.rest.repos.createRelease({
+			owner,
+			repo,
+			tag_name: appVersion,
+			name: appVersion
+		});
+
+		core.info(`Created release ${appVersion}`);
+		core.setOutput("released", true);
+		core.setOutput("html_url", data.html_url);
+		core.setOutput("upload_url", data.upload_url);
+		core.setOutput("release_id", data.id);
+		core.setOutput("release_tag", data.tag_name);
+		core.setOutput("release_name", data.name);
+	} else {
+		core.info("Version has not changed.");
+	}
 };
