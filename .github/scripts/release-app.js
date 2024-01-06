9f85ab61d53ac3c6c97e8fe26dd00c0d0fa07e64
DonColon <drrafshi@hotmail.de>
2024-01-06 02:27:17 +0100

    Get newest build artifact

--- .github/scripts/release-app.js
+++ .github/scripts/release-app.js
@@ -21,23 +21,36 @@ export default async ({ core, context, github }) => {
 		core.info(`First release version: ${appVersion}`);
 	}
 
-	if (isFirstRelease || appVersion > previousVersion) {
-		const { data } = await github.rest.repos.createRelease({
-			owner,
-			repo,
-			tag_name: appVersion,
-			name: appVersion,
-			target_commitish: process.env.GITHUB_SHA,
-		});
-
-		core.info(`Created release ${appVersion}`);
-		core.setOutput("released", true);
-		core.setOutput("html_url", data.html_url);
-		core.setOutput("upload_url", data.upload_url);
-		core.setOutput("release_id", data.id);
-		core.setOutput("release_tag", data.tag_name);
-		core.setOutput("release_name", data.name);
-	} else {
-		core.info("Version has not changed.");
+	if (!isFirstRelease && appVersion <= previousVersion) {
+		core.info("Version has not changed");
+		process.exit();
 	}
+
+	const { data } = await github.rest.repos.createRelease({
+		owner,
+		repo,
+		tag_name: appVersion,
+		name: appVersion,
+		target_commitish: process.env.GITHUB_SHA,
+	});
+
+	core.info(`Created release ${appVersion}`);
+	core.setOutput("released", true);
+	core.setOutput("html_url", data.html_url);
+	core.setOutput("upload_url", data.upload_url);
+	core.setOutput("release_id", data.id);
+	core.setOutput("release_tag", data.tag_name);
+	core.setOutput("release_name", data.name);
+
+	const artifactName = `${repo}-${appVersion}`;
+	core.info(`Download build artifact ${artifactName}`);
+
+	const { data: [buildArtifact] } = github.rest.actions.listArtifactsForRepo({
+		owner,
+		repo,
+		name: artifactName,
+		per_page: 1
+	});
+
+	console.log(buildArtifact);
 };
