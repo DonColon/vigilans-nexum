c9a5c8b0eff197b4b87286f5788ef984fb7ef75a
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-01-14 00:30:36 +0100

    Made changes

--- .github/scripts/download-release-asset.js
+++ .github/scripts/download-release-asset.js
@@ -1,11 +1,11 @@
 export default async ({ core, context, github }) => {
 	const { owner, repo } = context.repo;
 
-    const [ latestRelease ] = await github.rest.repos.listReleases({
+    const releases = await github.rest.repos.listReleases({
         owner,
         repo,
         per_page: 1
     });
 
-    console.log(latestRelease);
+    console.log(releases);
 };
