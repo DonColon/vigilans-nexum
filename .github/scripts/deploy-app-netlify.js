275d33759af9cae5c49b26afdfa160f41b6569f5
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-09 20:16:01 +0100

    Added environment secrets to script

--- .github/scripts/deploy-app-netlify.js
+++ .github/scripts/deploy-app-netlify.js
@@ -1,3 +1,8 @@
-export default async ({ core, context, github }) => {
-
+export default async ({ core, context, github, exec, netlify }) => {
+    core.info(netlify.site);
+    core.info(netlify.token);
+    // exec.exec("netlify deploy", [
+    //     `--dir dist`,
+    //     `--`
+    // ])
 };
