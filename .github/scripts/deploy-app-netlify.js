fed09ebd4383787d5b86b8adff34638a2b891597
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-09 20:26:50 +0100

    Execute netlify command

--- .github/scripts/deploy-app-netlify.js
+++ .github/scripts/deploy-app-netlify.js
@@ -1,8 +1,20 @@
 export default async ({ core, context, github, exec, netlify }) => {
-    core.info(netlify.site);
-    core.info(netlify.token);
-    // exec.exec("netlify deploy", [
-    //     `--dir dist`,
-    //     `--`
-    // ])
+    const options = {};
+
+    options.listeners = {
+        stdout: (data) => {
+            core.info(data.toString());
+        },
+        stderr: (data) => {
+            core.info(data.toString());
+        }
+    };
+
+    exec.exec("netlify deploy", [
+        `--dir dist`,
+        `--site ${netlify.site}`,
+        `--auth ${netlify.token}`
+    ]);
+
+    core.info("Finished deployment to netlify");
 };
