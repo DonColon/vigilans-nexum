4e255693f89e24ff2ca5ad2f876276864fc2d4c2
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-10 00:01:28 +0100

    Made changes

--- .github/scripts/deploy-app-netlify.js
+++ .github/scripts/deploy-app-netlify.js
@@ -3,14 +3,17 @@ export default async ({ core, context, github, exec, netlify }) => {
 
     options.listeners = {
         stdout: (data) => {
-            const deployInfo = JSON.parse(data.toString());
-            core.info(deployInfo.deploy_url);
+
         },
         stderr: (data) => {
             core.error(data.toString());
         }
     };
 
-    await exec.exec(`netlify deploy --dir dist --site ${netlify.site} --auth ${netlify.token} --json`, options);
+    const { stdout, stderr } = await exec.getExecOutput(`netlify deploy --dir dist --site ${netlify.site} --auth ${netlify.token} --json`, options);
+
+    const deployInfo = JSON.parse(stdout);
+    core.info(deployInfo.deploy_url);
+    
     core.info("Finished deployment to netlify");
 };
