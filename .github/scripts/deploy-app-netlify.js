2ca436cec6f03c59cc4cf59a7357e184a2952f05
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-10 00:13:00 +0100

    Dynamically set url of environment

--- .github/scripts/deploy-app-netlify.js
+++ .github/scripts/deploy-app-netlify.js
@@ -1,17 +1,9 @@
 export default async ({ core, context, github, exec, netlify }) => {
-    const options = {};
-
-    options.listeners = {
-        stdout: (data) => {
-
-        },
-        stderr: (data) => {
-            core.error(data.toString());
-        }
-    };
-
-    const { stdout } = await exec.getExecOutput(`netlify deploy --dir dist --site ${netlify.site} --auth ${netlify.token} --json`, options);
+    const command = `netlify deploy --dir dist --site ${netlify.site} --auth ${netlify.token} --json`;
+    const { stdout } = await exec.getExecOutput(command);
 
     const deployInfo = JSON.parse(stdout);
     core.info(`\nDeployed app at url: ${deployInfo.deploy_url}`);
+    
+    return deployInfo;
 };
