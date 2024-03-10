59330330a17ed88031f56d702a0966ccbfd28ff6
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-10 02:11:37 +0100

    Published all deploy info as output

--- .github/scripts/deploy-app-netlify.js
+++ .github/scripts/deploy-app-netlify.js
@@ -2,8 +2,11 @@ export default async ({ core, context, github, exec, netlify }) => {
     const command = `netlify deploy --dir dist --site ${netlify.site} --auth ${netlify.token} --json`;
     const { stdout } = await exec.getExecOutput(command);
 
-    const deployInfo = JSON.parse(stdout);
-    core.info(`\nDeployed app at url: ${deployInfo.deploy_url}`);
+    const { site_name, deploy_id, deploy_url, logs } = JSON.parse(stdout);
+    core.info(`\nDeployed app at url: ${deploy_url}`);
     
-    return deployInfo.deploy_url;
+    core.setOutput("siteName", site_name);
+	core.setOutput("deployID", deploy_id);
+	core.setOutput("deployUrl", deploy_url);
+	core.setOutput("logs", logs);
 };
