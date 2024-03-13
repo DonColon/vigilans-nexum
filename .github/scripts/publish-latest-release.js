6f1622721b284137aee0a9657199f0d10b4698b2
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-14 00:46:59 +0100

    Publish release based on branch

--- .github/scripts/publish-latest-release.js
+++ .github/scripts/publish-latest-release.js
@@ -1,15 +1,13 @@
 export default async ({ core, context, github, release }) => {
 	const { owner, repo } = context.repo;
 
-	console.log(process.env);
+	await github.rest.repos.updateRelease({
+		owner,
+		repo,
+		release_id: release.id,
+		draft: process.env.GITHUB_REF_NAME.includes("feature"),
+		prerelease: process.env.GITHUB_REF_NAME === "dev"
+	});
 
-	// await github.rest.repos.updateRelease({
-	// 	owner,
-	// 	repo,
-	// 	release_id: release.id,
-	// 	draft: false,
-	// 	prerelease: false
-	// });
-
-	// core.info(`Published release ${release.name}`);
+	core.info(`Published release ${release.name}`);
 };
