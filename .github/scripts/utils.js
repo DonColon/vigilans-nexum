fc5654c1beb78a5256477ae0921e4c0e635c08e5
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-15 01:07:38 +0100

    Added utils

---
+++ .github/scripts/utils.js
@@ -0,0 +1,7 @@
+export function formatRepositoryTitle(repo) {
+	return repo.split("-").map(value => capitalize(value)).join(" ");
+}
+
+export function capitalize(value) {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
