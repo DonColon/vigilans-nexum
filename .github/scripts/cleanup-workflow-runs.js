02f7c835484ab9c3f998e9cee5a3ae202da4bb8e
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-16 23:35:58 +0100

    test

--- .github/scripts/cleanup-workflow-runs.js
+++ .github/scripts/cleanup-workflow-runs.js
@@ -1,23 +1,24 @@
 export default async ({ core, context, github }) => {
     const { owner, repo } = context.repo;
 
-    const { data: { workflow_runs: workflowRuns } } = await github.rest.actions.listWorkflowRunsForRepo({
+    const reponse = await github.rest.actions.listWorkflowRunsForRepo({
         owner,
         repo,
-        status: ["completed"]
+        status: "completed",
+        per_page: 100
     });
 
-    console.log(workflowRuns);
+    console.log(reponse)
 
-    core.info(`${workflowRuns.length} workflow runs found`);
+    // core.info(`${workflowRuns.length} workflow runs found`);
 
-    for(const workflowRun of workflowRuns) {
-        await github.rest.actions.deleteWorkflowRun({
-            owner,
-            repo,
-            run_id: workflowRun.id,
-        });
-    }
+    // for(const workflowRun of workflowRuns) {
+    //     await github.rest.actions.deleteWorkflowRun({
+    //         owner,
+    //         repo,
+    //         run_id: workflowRun.id,
+    //     });
+    // }
 
     core.info(`Deleted all workflow runs`);
 };
