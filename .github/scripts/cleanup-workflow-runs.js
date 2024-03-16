fad83afbf60d2673862ae3fbb2662e941d5b9483
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-17 00:14:53 +0100

    Fixed issue

--- .github/scripts/cleanup-workflow-runs.js
+++ .github/scripts/cleanup-workflow-runs.js
@@ -10,17 +10,17 @@ export default async ({ core, context, github }) => {
 
     for await(const response of github.paginate.iterator(github.rest.actions.listWorkflowRunsForRepo, parameters)) {
         console.log(response)
-        // const { data: { workflow_runs: workflowRuns } } = response;
+        const { data: workflowRuns } = response;
 
-        // core.info(`${workflowRuns.length} workflow runs found`);
+        core.info(`${workflowRuns.length} workflow runs found`);
 
-        // for(const workflowRun of workflowRuns) {
-        //     await github.rest.actions.deleteWorkflowRun({
-        //         owner,
-        //         repo,
-        //         run_id: workflowRun.id,
-        //     });
-        // }
+        for(const workflowRun of workflowRuns) {
+            await github.rest.actions.deleteWorkflowRun({
+                owner,
+                repo,
+                run_id: workflowRun.id,
+            });
+        }
     }
 
     core.info(`Deleted all workflow runs`);
