3316be39f99811fd25e4cbf044fddba645c359dd
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2024-03-17 00:11:55 +0100

    Added pagination

--- .github/scripts/cleanup-workflow-runs.js
+++ .github/scripts/cleanup-workflow-runs.js
@@ -1,21 +1,26 @@
 export default async ({ core, context, github }) => {
     const { owner, repo } = context.repo;
 
+    const parameters = {
+        owner,
+        repo,
+        per_page: 100,
+        status: "completed"
+    }
 
+    for await(const response of github.paginate.iterator(github.rest.actions.listWorkflowRunsForRepo, parameters)) {
+        const { data: { workflow_runs: workflowRuns } } = response;
 
+        core.info(`${workflowRuns.length} workflow runs found`);
 
-
-    console.log(github)
-
-    // core.info(`${workflowRuns.length} workflow runs found`);
-
-    // for(const workflowRun of workflowRuns) {
-    //     await github.rest.actions.deleteWorkflowRun({
-    //         owner,
-    //         repo,
-    //         run_id: workflowRun.id,
-    //     });
-    // }
+        for(const workflowRun of workflowRuns) {
+            await github.rest.actions.deleteWorkflowRun({
+                owner,
+                repo,
+                run_id: workflowRun.id,
+            });
+        }
+    }
 
     core.info(`Deleted all workflow runs`);
 };
