59dfa209fe187e5c03e10b101a160d3779d78a25
DonColon <dardan.rrafshi@gmail.com>
2025-02-26 22:30:39 +0100

    Add unit tests for World class to validate component and entity management

--- src/core/ecs/World.ts
+++ src/core/ecs/World.ts
@@ -1,1 +1,220 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */import { GameError } from "core/GameError";import { JsonSchema } from "./JsonSchema";import { ComponentConstructor } from "./Component";import { Entity, EntityType } from "./Entity";import { System, SystemConstructor } from "./System";import { UpdateSystem } from "./UpdateSystem";import { RenderSystem } from "./RenderSystem";import { GameStateConstructor } from "core/GameState";export class World {	private components: Map<string, ComponentConstructor<any>>;	private states: Map<string, GameStateConstructor>;	private entities: Map<string, Entity>;	private systems: Map<string, System>;	private updateSchedule: System[];	private renderSchedule: System[];	constructor() {		this.components = new Map<string, ComponentConstructor<any>>();		this.states = new Map<string, GameStateConstructor>();		this.entities = new Map<string, Entity>();		this.systems = new Map<string, System>();		this.updateSchedule = [];		this.renderSchedule = [];	}	public update(elapsed: number, frame: number) {		for (const system of this.updateSchedule) {			if (system.isEnabled()) {				system.execute(elapsed, frame);			}		}	}	public render(elapsed: number, frame: number) {		for (const system of this.renderSchedule) {			if (system.isEnabled()) {				system.execute(elapsed, frame);			}		}	}	public registerComponent<T extends JsonSchema>(componentType: ComponentConstructor<T>): this {		if (this.components.has(componentType.name)) {			throw new GameError(`Component ${componentType.name} is already registered`);		}		this.components.set(componentType.name, componentType);		return this;	}	public unregisterComponent<T extends JsonSchema>(componentType: ComponentConstructor<T>): this {		this.components.delete(componentType.name);		return this;	}	public getComponent(name: string): ComponentConstructor<any> {		const component = this.components.get(name);		if (component === undefined) {			throw new GameError(`Component ${name} is not registered`);		}		return component;	}	public getComponents(): ComponentConstructor<any>[] {		return Array.from(this.components.values());	}	public hasComponent<T extends JsonSchema>(componentType: ComponentConstructor<T>): boolean {		return this.components.has(componentType.name);	}	public registerEntityState(stateType: GameStateConstructor): this {		if (this.states.has(stateType.name)) {			throw new GameError(`Entity State ${stateType.name} is already registered`);		}		this.states.set(stateType.name, stateType);		return this;	}	public unregisterEntityState(stateType: GameStateConstructor): this {		this.states.delete(stateType.name);		return this;	}	public getEntityState(name: string): GameStateConstructor {		const state = this.states.get(name);		if (state === undefined) {			throw new GameError(`Entity State ${name} is not registered`);		}		return state;	}	public getEntityStates(): GameStateConstructor[] {		return Array.from(this.states.values());	}	public hasEntityState(stateType: GameStateConstructor): boolean {		return this.states.has(stateType.name);	}	public registerEntity(entityType: EntityType): this {		if (this.entities.has(entityType.id)) {			throw new GameError(`Entity ${entityType.id} already exists`);		}		const entity = Entity.parse(entityType);		this.entities.set(entity.getID(), entity);		return this;	}	public createEntity(id?: string): Entity {		if (id && this.entities.has(id)) {			throw new GameError(`Entity ${id} already exists`);		}		const entity = new Entity(id);		this.entities.set(entity.getID(), entity);		return entity;	}	public unregisterEntity(entity: Entity): this {		this.entities.delete(entity.getID());		return this;	}	public getEntity(id: string): Entity {		const entity = this.entities.get(id);		if (entity === undefined) {			throw new GameError(`Entity ${id} does not exist`);		}		return entity;	}	public getEntities(): Entity[] {		return Array.from(this.entities.values());	}	public hasEntity(entity: Entity): boolean {		return this.entities.has(entity.getID());	}	public registerSystem(systemType: SystemConstructor, priority: number): this {		if (this.components.has(systemType.name)) {			throw new GameError(`System ${systemType.name} is already registered`);		}		const system = new systemType(priority);		this.systems.set(systemType.name, system);		if (system instanceof UpdateSystem) {			this.scheduleUpdateSystems();		} else if (system instanceof RenderSystem) {			this.scheduleRenderSystems();		}		return this;	}	private scheduleUpdateSystems() {		const systems = Array.from(this.systems.values());		this.updateSchedule = systems.filter((system) => system instanceof UpdateSystem);		this.updateSchedule.sort(System.byPriority);	}	private scheduleRenderSystems() {		const systems = Array.from(this.systems.values());		this.renderSchedule = systems.filter((system) => system instanceof RenderSystem);		this.renderSchedule.sort(System.byPriority);	}	public unregisterSystem(systemType: SystemConstructor): this {		this.systems.delete(systemType.name);		return this;	}	public getSystem(systemType: SystemConstructor): System {		const system = this.systems.get(systemType.name);		if (system === undefined) {			throw new GameError(`System ${systemType.name} is not registered`);		}		return system;	}	public getUpdateSchedule(): System[] {		return this.updateSchedule;	}	public getRenderSchedule(): System[] {		return this.renderSchedule;	}	public hasSystem(systemType: SystemConstructor): boolean {		return this.systems.has(systemType.name);	}}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { GameError } from "core/GameError";
+import { JsonSchema } from "./JsonSchema";
+import { ComponentConstructor } from "./Component";
+import { Entity, EntityType } from "./Entity";
+import { System, SystemConstructor } from "./System";
+import { UpdateSystem } from "./UpdateSystem";
+import { RenderSystem } from "./RenderSystem";
+import { GameStateConstructor } from "core/GameState";
+
+export class World {
+	private components: Map<string, ComponentConstructor<any>>;
+	private states: Map<string, GameStateConstructor>;
+	private entities: Map<string, Entity>;
+	private systems: Map<string, System>;
+
+	private updateSchedule: System[];
+	private renderSchedule: System[];
+
+	constructor() {
+		this.components = new Map<string, ComponentConstructor<any>>();
+		this.states = new Map<string, GameStateConstructor>();
+		this.entities = new Map<string, Entity>();
+		this.systems = new Map<string, System>();
+
+		this.updateSchedule = [];
+		this.renderSchedule = [];
+	}
+
+	public update(elapsed: number, frame: number) {
+		for (const system of this.updateSchedule) {
+			if (system.isEnabled()) {
+				system.execute(elapsed, frame);
+			}
+		}
+	}
+
+	public render(elapsed: number, frame: number) {
+		for (const system of this.renderSchedule) {
+			if (system.isEnabled()) {
+				system.execute(elapsed, frame);
+			}
+		}
+	}
+
+	public registerComponent<T extends JsonSchema>(componentType: ComponentConstructor<T>): this {
+		if (this.hasComponent(componentType)) {
+			throw new GameError(`Component ${componentType.name} is already registered`);
+		}
+
+		this.components.set(componentType.name, componentType);
+		return this;
+	}
+
+	public unregisterComponent<T extends JsonSchema>(componentType: ComponentConstructor<T>): this {
+		this.components.delete(componentType.name);
+		return this;
+	}
+
+	public getComponent(name: string): ComponentConstructor<any> {
+		const component = this.components.get(name);
+
+		if (component === undefined) {
+			throw new GameError(`Component ${name} is not registered`);
+		}
+
+		return component;
+	}
+
+	public getComponents(): ComponentConstructor<any>[] {
+		return Array.from(this.components.values());
+	}
+
+	public hasComponent<T extends JsonSchema>(componentType: ComponentConstructor<T> | string): boolean {
+		const componentName = (typeof componentType === "string") ? componentType : componentType.name;
+		return this.components.has(componentName);
+	}
+
+	public registerEntityState(stateType: GameStateConstructor): this {
+		if (this.hasEntityState(stateType)) {
+			throw new GameError(`Entity State ${stateType.name} is already registered`);
+		}
+
+		this.states.set(stateType.name, stateType);
+		return this;
+	}
+
+	public unregisterEntityState(stateType: GameStateConstructor): this {
+		this.states.delete(stateType.name);
+		return this;
+	}
+
+	public getEntityState(name: string): GameStateConstructor {
+		const state = this.states.get(name);
+
+		if (state === undefined) {
+			throw new GameError(`Entity State ${name} is not registered`);
+		}
+
+		return state;
+	}
+
+	public getEntityStates(): GameStateConstructor[] {
+		return Array.from(this.states.values());
+	}
+
+	public hasEntityState(stateType: GameStateConstructor | string): boolean {
+		const stateName = (typeof stateType === "string") ? stateType : stateType.name;
+		return this.states.has(stateName);
+	}
+
+	public registerEntity(entityType: EntityType): this {
+		if (this.hasEntity(entityType)) {
+			throw new GameError(`Entity ${entityType.id} already exists`);
+		}
+
+		const entity = Entity.parse(entityType);
+		this.entities.set(entity.getID(), entity);
+		return this;
+	}
+
+	public createEntity(id?: string): Entity {
+		if (id && this.hasEntity(id)) {
+			throw new GameError(`Entity ${id} already exists`);
+		}
+
+		const entity = new Entity(id);
+		this.entities.set(entity.getID(), entity);
+		return entity;
+	}
+
+	public unregisterEntity(entity: Entity): this {
+		this.entities.delete(entity.getID());
+		return this;
+	}
+
+	public getEntity(id: string): Entity {
+		const entity = this.entities.get(id);
+
+		if (entity === undefined) {
+			throw new GameError(`Entity ${id} does not exist`);
+		}
+
+		return entity;
+	}
+
+	public getEntities(): Entity[] {
+		return Array.from(this.entities.values());
+	}
+
+	public hasEntity(entity: Entity| EntityType | string): boolean {
+		let entityID;
+
+		if(entity instanceof Entity) {
+			entityID = entity.getID();
+		} else if (typeof entity === "string") {
+			entityID = entity;
+		} else {
+			entityID = entity.id;
+		}
+
+		return this.entities.has(entityID);
+	}
+
+	public registerSystem(systemType: SystemConstructor, priority: number): this {
+		if (this.hasSystem(systemType)) {
+			throw new GameError(`System ${systemType.name} is already registered`);
+		}
+
+		const system = new systemType(priority);
+		this.systems.set(systemType.name, system);
+
+		this.scheduleUpdateSystems();
+		this.scheduleRenderSystems();
+		return this;
+	}
+
+	public unregisterSystem(systemType: SystemConstructor): this {
+		this.systems.delete(systemType.name);
+		this.scheduleUpdateSystems();
+		this.scheduleRenderSystems();
+		return this;
+	}
+
+	private scheduleUpdateSystems() {
+		const systems = Array.from(this.systems.values());
+		this.updateSchedule = systems.filter((system) => system instanceof UpdateSystem);
+		this.updateSchedule.sort(System.byPriority);
+	}
+
+	private scheduleRenderSystems() {
+		const systems = Array.from(this.systems.values());
+		this.renderSchedule = systems.filter((system) => system instanceof RenderSystem);
+		this.renderSchedule.sort(System.byPriority);
+	}
+
+	public getSystem(systemType: SystemConstructor): System {
+		const system = this.systems.get(systemType.name);
+
+		if (system === undefined) {
+			throw new GameError(`System ${systemType.name} is not registered`);
+		}
+
+		return system;
+	}
+
+	public getUpdateSchedule(): System[] {
+		return this.updateSchedule;
+	}
+
+	public getRenderSchedule(): System[] {
+		return this.renderSchedule;
+	}
+
+	public hasSystem(systemType: SystemConstructor | string): boolean {
+		const systemName = (typeof systemType === "string") ? systemType : systemType.name;
+		return this.systems.has(systemName);
+	}
+}
---
+++ test/core/ecs/World.spec.ts
@@ -0,0 +1,262 @@
+import { test, expect, suite, vi } from "vitest";
+import { World } from "../../../src/core/ecs/World";
+import { Component } from "../../../src/core/ecs/Component";
+import { GameError } from "../../../src/core/GameError";
+import { GameState } from "../../../src/core/GameState";
+import { EntityType } from "../../../src/core/ecs/Entity";
+import { Query } from "../../../src/core/ecs/Query";
+import { EventSystem } from "../../../src/core/events/EventSystem";
+import { UpdateSystem } from "../../../src/core/ecs/UpdateSystem";
+import { RenderSystem } from "../../../src/core/ecs/RenderSystem";
+
+declare global {
+    var world: World;
+    var eventSystem: EventSystem;
+}
+
+suite("World Test Suite", () => {
+    class PointComponent extends Component<{
+        x: number;
+        y: number;
+    }> {}
+
+    class JumpState extends GameState {
+        onEnter() {}
+        onExit() {}
+    }
+
+    class MovementSystem extends UpdateSystem {
+        queries = {
+            query: new Query({
+                allowlist: [],
+                blocklist: []
+            })
+        };
+
+        public initialize(): void {}
+        public execute(): void {}
+    }
+
+    class AttackSystem extends UpdateSystem {
+        queries = {
+            query: new Query({
+                allowlist: [],
+                blocklist: []
+            })
+        };
+
+        public initialize(): void {}
+        public execute(): void {}
+    }
+
+    class MapSystem extends RenderSystem {
+		queries = {
+			query: new Query({
+				allowlist: [],
+				blocklist: []
+			})
+		};
+
+		public initialize(): void {}
+		public execute(): void {}
+	}
+
+    test("Update the world one frame", () => {
+        const world = new World();
+        globalThis.world = world;
+
+        const eventSystem = new EventSystem();
+        globalThis.eventSystem = eventSystem;
+
+        world.registerSystem(MovementSystem, 0);
+        world.registerSystem(MapSystem, 0);
+
+        const movementSystem = world.getSystem(MovementSystem);
+        const mapSystem = world.getSystem(MapSystem);
+
+        const movementSystemExecute = vi.spyOn(movementSystem, "execute");
+        const mapSystemExecute = vi.spyOn(mapSystem, "execute");
+
+        world.update(20, 10);
+        world.render(20, 10);
+
+        expect(movementSystemExecute).toHaveBeenCalledTimes(1);
+        expect(mapSystemExecute).toHaveBeenCalledTimes(1);
+
+        movementSystem.disable();
+        mapSystem.disable();
+
+        world.update(20, 10);
+        world.render(20, 10);
+
+        expect(movementSystemExecute).toHaveBeenCalledTimes(1);
+        expect(mapSystemExecute).toHaveBeenCalledTimes(1);
+    });
+
+    test("Register a component to the world", () => {
+        const world = new World();
+        world.registerComponent(PointComponent);
+        
+        expect(() => world.registerComponent(PointComponent)).toThrowError(GameError);
+        expect(world.getComponent(PointComponent.name)).toEqual(PointComponent);
+        expect(() => world.getComponent("MoveComponent")).toThrowError(GameError);
+
+        const components = world.getComponents();
+        expect(components[0]).toEqual(PointComponent);
+    });
+
+    test("Unregister a component from the world", () => {
+        const world = new World();
+        world.registerComponent(PointComponent);
+        world.unregisterComponent(PointComponent);
+
+        expect(() => world.getComponent(PointComponent.name)).toThrowError(GameError);
+        
+        const components = world.getComponents();
+        expect(components).toHaveLength(0);
+    });
+
+    test("World has a component", () => {
+        const world = new World();
+        world.registerComponent(PointComponent);
+
+        expect(world.hasComponent(PointComponent)).toBeTruthy();
+        expect(world.hasComponent("PointComponent")).toBeTruthy();
+    });
+
+    test("Register an entity state to the world", () => {
+        const world = new World();
+        world.registerEntityState(JumpState);
+
+        expect(() => world.registerEntityState(JumpState)).toThrowError(GameError);
+        expect(world.getEntityState(JumpState.name)).toEqual(JumpState);
+        expect(() => world.getEntityState("FallState")).toThrowError(GameError);
+
+        const states = world.getEntityStates();
+        expect(states[0]).toEqual(JumpState);
+    });
+
+    test("Unregister an entity state from the world", () => {
+        const world = new World();
+        world.registerEntityState(JumpState);
+        world.unregisterEntityState(JumpState);
+
+        expect(() => world.getEntityState(JumpState.name)).toThrowError(GameError);
+        
+        const states = world.getEntityStates();
+        expect(states).toHaveLength(0);
+    });
+
+    test("World has an entity state", () => {
+        const world = new World();
+        world.registerEntityState(JumpState);
+
+        expect(world.hasEntityState(JumpState)).toBeTruthy();
+        expect(world.hasEntityState("JumpState")).toBeTruthy();
+    });
+
+    test("Create an entity to the world", () => {
+        const world = new World();
+        const entity = world.createEntity("1337");
+        expect(() => world.createEntity("1337")).toThrowError(GameError);
+        
+        expect(world.getEntity("1337")).toEqual(entity);
+        expect(() => world.getEntity("1338")).toThrowError(GameError);
+
+        const entities = world.getEntities();
+        expect(entities[0]).toEqual(entity);
+    });
+
+    test("Destroy an entity from the world", () => {
+        const world = new World();
+        const entity = world.createEntity("1337");
+        world.unregisterEntity(entity);
+
+        expect(() => world.getEntity("1337")).toThrowError(GameError);
+
+        const entities = world.getEntities();
+        expect(entities).toHaveLength(0);
+    });
+
+    test("World has an entity", () => {
+        const world = new World();
+        const entity = world.createEntity("1337");
+
+        const entityType: EntityType = {
+            id: "1337",
+            enabled: true,
+            components: {},
+            states: []
+        }
+
+        expect(world.hasEntity(entity)).toBeTruthy();
+        expect(world.hasEntity(entityType)).toBeTruthy();
+        expect(world.hasEntity("1337")).toBeTruthy();
+    });
+
+    test("Register an entity to the world", () => {
+        const world = new World();
+
+        const entityType: EntityType = {
+            id: "1337",
+            enabled: true,
+            components: {},
+            states: []
+        };
+
+        world.registerEntity(entityType);
+        expect(() => world.registerEntity(entityType)).toThrowError(GameError);
+
+        const entity = world.getEntity("1337");
+        expect(world.hasEntity(entity)).toBeTruthy();
+        expect(() => world.getEntity("1338")).toThrowError(GameError);
+
+        const entities = world.getEntities();
+        expect(world.hasEntity(entities[0])).toBeTruthy();
+    });
+
+    test("Register a system to the world", () => {
+        const world = new World();
+        globalThis.world = world;
+
+        const eventSystem = new EventSystem();
+        globalThis.eventSystem = eventSystem;
+
+        world.registerSystem(MapSystem, 0);
+        world.registerSystem(MovementSystem, 0);
+        expect(() => world.registerSystem(MovementSystem, 0)).toThrowError(GameError);
+
+        expect(world.getSystem(MovementSystem)).toBeInstanceOf(MovementSystem);
+        expect(() => world.getSystem(AttackSystem)).toThrowError(GameError);
+        
+        const updateSystems = world.getUpdateSchedule();
+        expect(updateSystems[0]).toBeInstanceOf(MovementSystem);
+
+        const renderSystems = world.getRenderSchedule();
+        expect(renderSystems[0]).toBeInstanceOf(MapSystem);
+    });
+
+    test("Unregister a system from the world", () => {
+        const world = new World();
+        globalThis.world = world;
+
+        const eventSystem = new EventSystem();
+        globalThis.eventSystem = eventSystem;
+
+        world.registerSystem(MovementSystem, 0);
+        world.unregisterSystem(MovementSystem);
+
+        expect(() => world.getSystem(MovementSystem)).toThrowError(GameError);
+
+        const updateSystems = world.getUpdateSchedule();
+        expect(updateSystems).toHaveLength(0);
+    });
+
+    test("World has a system", () => {
+        const world = new World();
+        world.registerSystem(MovementSystem, 0);
+
+        expect(world.hasSystem(MovementSystem)).toBeTruthy();
+        expect(world.hasSystem("MovementSystem")).toBeTruthy();
+    });
+});
