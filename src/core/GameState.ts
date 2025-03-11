31f2e1db2e6c087edba0559caa285be387ad57cd
DonColon <dardan.rrafshi@gmail.com>
2025-03-11 03:54:03 +0100

    Refactor GameState, Component, System, Query, and Randomizer classes for improved type definitions and code clarity

--- src/core/GameState.ts
+++ src/core/GameState.ts
@@ -1,1 +1,6 @@
-export interface GameStateConstructor {	new (): GameState;}export abstract class GameState {	public abstract onEnter(): void;	public abstract onExit(): void;}
+export type GameStateConstructor = new () => GameState;
+
+export abstract class GameState {
+	public abstract onEnter(): void;
+	public abstract onExit(): void;
+}
